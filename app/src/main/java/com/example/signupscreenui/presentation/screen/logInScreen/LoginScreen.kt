c69e4f3f92b454ed888154a661a52f7b9c842d0f
Anil <chaudharyanil5656@gmail.com>
2026-01-01 00:08:18 +0545

    optimized loginScreen

--- app/src/main/java/com/example/signupscreenui/presentation/screen/logInScreen/LoginScreen.kt
+++ app/src/main/java/com/example/signupscreenui/presentation/screen/logInScreen/LoginScreen.kt
@@ -1,5 +1,8 @@
 package com.example.signupscreenui.presentation.screen.logInScreen
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -18,9 +21,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
@@ -30,6 +35,8 @@ fun LoginScreenUi(
     state: LoginState,
     event: (LoginEvent) -> Unit
 ) {
+    val context = LocalContext.current
+
     Scaffold { padding ->
         Column(
             modifier = Modifier
@@ -40,40 +47,50 @@ fun LoginScreenUi(
             verticalArrangement = Arrangement.spacedBy(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(Modifier.height(20.dp))
-            Text("Login", style = MaterialTheme.typography.headlineLarge)
-            Spacer(Modifier.height(100.dp))
-            EmailField(
-                value = state.email,
-                onChange = { event(LoginEvent.EmailChanged(it)) }
-            )
-
-            PasswordField(value = state.password) { event(LoginEvent.PasswordChanged(it)) }
-
-            Button(
-                onClick = { event(LoginEvent.Login) },
-                enabled = !state.isLoading,
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
             ) {
-                if (state.isLoading) {
-                    CircularProgressIndicator(Modifier.size(20.dp), strokeWidth = 2.dp)
-                } else {
-                    Text("Login")
+                Spacer(Modifier.height(20.dp))
+                Text("Login", style = MaterialTheme.typography.headlineLarge)
+                Spacer(Modifier.height(100.dp))
+                EmailField(
+                    value = state.email,
+                    onChange = { event(LoginEvent.EmailChanged(it)) }
+                )
+
+                PasswordField(value = state.password) { event(LoginEvent.PasswordChanged(it)) }
+
+                Button(
+                    onClick = { event(LoginEvent.Login) },
+                    enabled = !state.isLoading,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    if (state.isLoading) {
+                        CircularProgressIndicator(Modifier.size(20.dp), strokeWidth = 2.dp)
+                    } else {
+                        Text("Login")
+                    }
                 }
+                Text("Forgot Password?", textDecoration = TextDecoration.Underline)
             }
-            Text("Forgot Password?", textDecoration = TextDecoration.Underline)
 
-            Text("Don't have an account?")
-            TextButton(
-                onClick = { }
-            ) {
+            Column {
+                Text("Don't have an account?")
                 Text(
                     "Sign Up",
                     style = TextStyle(
                         textDecoration = TextDecoration.Underline,
                         color = Color.Red
-                    )
+                    ),
+                    modifier = Modifier
+                        .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null
+                        ) {
+                            Toast.makeText(context, "Sign Up Clicked!", Toast.LENGTH_SHORT).show()
+                        }
                 )
             }
         }
