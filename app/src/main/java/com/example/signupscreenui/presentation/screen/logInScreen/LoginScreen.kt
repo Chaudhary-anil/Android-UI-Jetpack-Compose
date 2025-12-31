e0d22bce2b0757c37a0fed6380c77446e946ff20
Anil <chaudharyanil5656@gmail.com>
2025-12-31 22:22:21 +0545

    Login Screen added

---
+++ app/src/main/java/com/example/signupscreenui/presentation/screen/logInScreen/LoginScreen.kt
@@ -0,0 +1,19 @@
+package com.example.signupscreenui.presentation.screen.logInScreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreenUi(modifier: Modifier = Modifier) {
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {  }
+    }
+}
